28b917771c7b7b413d9ae5997e436437766b70cb
mesotron.dev <mail@mesotron.dev>
2025-05-02 14:56:38 +0000

    trim lines
    FossilOrigin-Name: 3619a9fea7b6d62272d7a7f27c295db262e6a0576c65ecde6fc0461717dd40ea

--- quarryforge/exceptions/model_exception/repo_config_exception.py
+++ quarryforge/exceptions/model_exception/repo_config_exception.py
@@ -11,101 +11,88 @@ class UserTypeError(RepoConfigError, TypeError):
     """Raise for invalid user type."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.missing_user()
-        )
+            message or repo_config_error_conf.invalid_user())
 
 
 class EmailTypeError(RepoConfigError, TypeError):
     """Raise for invalid email type."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.missing_email()
-        )
+            message or repo_config_error_conf.invalid_email())
 
 
 class SrcRepoTypeError(RepoConfigError, TypeError):
     """Raise for invalid src_repo type."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.missing_src()
-        )
+            message or repo_config_error_conf.invalid_src())
 
 
 class UpdateRepoTypeError(RepoConfigError, TypeError):
     """Raise for invalid update_repo type."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.missing_update()
-        )
+            message or repo_config_error_conf.invalid_update())
 
 
 class UpdateDirTypeError(RepoConfigError, TypeError):
     """Raise for invalid update_dir type."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.missing_update_dir()
-        )
+            message or repo_config_error_conf.invalid_update_dir())
 
 
 class TemplateTypeError(RepoConfigError, TypeError):
     """Raise for invalid template type."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.missing_update_dir()
-        )
+            message or repo_config_error_conf.invalid_update_dir())
 
 
 class ProjectNameTypeError(RepoConfigError, TypeError):
     """Raise for invalid project_name type."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.project_name_invalid()
-        )
+            message or repo_config_error_conf.project_name_invalid())
 
 
 class ProjectDescTypeError(RepoConfigError, TypeError):
     """Raise for invalid project_desc type."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.project_desc_invalid()
-        )
+            message or repo_config_error_conf.project_desc_invalid())
 
 
 class UserValueError(RepoConfigError, ValueError):
     """Raise for an empty user value."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.empty_user()
-        )
+            message or repo_config_error_conf.empty_user())
 
 
 class EmailValueError(RepoConfigError, ValueError):
     """Raise for an empty email value."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.empty_email()
-        )
+            message or repo_config_error_conf.empty_email())
 
 
 class ProjectNameValueError(RepoConfigError, ValueError):
     """Raise for an empty user value."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.empty_project_name()
-        )
+            message or repo_config_error_conf.empty_project_name())
 
 
 class ProjectDescValueError(RepoConfigError, ValueError):
     """Raise for an empty user value."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.empty_project_desc()
-        )
+            message or repo_config_error_conf.empty_project_desc())
 
 
 class ImmutableRepoConfigError(RepoConfigError):
     """Raise for any attempt to add or set attributes on RepoConfig."""
     def __init__(self, message=None):
         super().__init__(
-            message or repo_config_error_conf.immutable_config()
-        )
+            message or repo_config_error_conf.immutable_config())
