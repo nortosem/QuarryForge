1495036774a8d5ffeb0a2a877cd0e0c466c24fe8
mesotron.dev <mail@mesotron.dev>
2025-05-03 01:08:32 +0000

    updated with message utils, linted
    FossilOrigin-Name: e924c23f0a2021036edf00e9b0c87f63fcadef2384c7b129ce6ddfdd81e9e222

--- quarryforge/exceptions/model_exception/args_exception.py
+++ quarryforge/exceptions/model_exception/args_exception.py
@@ -2,179 +2,183 @@
 
 
 """
-import quarryforge.config.exception_conf.models_exception.args_error_conf
-from quarryforge.exceptions.model_exception.base_models_exception
+from quarryforge.config.exception_conf.models_exception \
+    import args_config_error_conf
+from quarryforge.exceptions.model_exception import base_models_exception
 
 
-class InfoVersionTypeError(base_models_exception.InfoArgsError):
+class InfoVersionTypeError(base_models_exception.InfoArgsError, TypeError):
     """Raise for invalid version type"""
     def __init__(self, message=None):
         super().__init__(
             message or args_config_error_conf.invalid_info_version())
 
 
-class InfoRepoTypeError(base_models_exception.InfoArgsError):
+class InfoRepoTypeError(base_models_exception.InfoArgsError, TypeError):
     """Raise for invalid src_repo type"""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.invalid_info_repo())
+            message or args_config_error_conf.invalid_info_repo()
+        )
 
-
-class InfoVersionValueError(base_models_exception.InfoArgsError):
+class InfoVersionValueError(base_models_exception.InfoArgsError, ValueError):
     """Raise for empty version"""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.empty_info_version())
+            message or args_config_error_conf.empty_info_version()
+        )
 
-
-class InfoRepoValueError(base_models_exception.InfoArgsError):
+class InfoRepoValueError(base_models_exception.InfoArgsError, ValueError):
     """Raise for empty src_repoe"""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.empty_info_repo())
+            message or args_config_error_conf.empty_info_repo()
+        )
 
-
-class ImmutableInfoArgsError(base_models_exception.InfoArgsError):
+class ImmutableInfoArgsError(base_models_exception.InfoArgsError, TypeError):
     """Raise for any attempt to add or set attributes on InfoArgs."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.immutable_info_config())
+            message or args_config_error_conf.immutable_info_config()
+        )
 
-
-class DiffParentTypeError(base_models_exception.DiffArgsError):
+class DiffParentTypeError(base_models_exception.DiffArgsError, TypeError):
     """Raise for invalid parent type"""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.invalid_diff_parent())
+            message or args_config_error_conf.invalid_diff_parent()
+        )
 
-
-class DiffParentValueError(base_models_exception.DiffArgsError):
+class DiffParentValueError(base_models_exception.DiffArgsError, ValueError):
     """Raise for empty parent type"""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.empty_diff_parent())
+            message or args_config_error_conf.empty_diff_parent()
+        )
 
-
-class DiffChildTypeError(base_models_exception.DiffArgsError):
+class DiffChildTypeError(base_models_exception.DiffArgsError, TypeError):
     """Raise for invalid child type"""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.invalid_diff_child())
+            message or args_config_error_conf.invalid_diff_child()
+        )
 
-
-class DiffChildValueError(base_models_exception.DiffArgsError):
+class DiffChildValueError(base_models_exception.DiffArgsError, ValueError):
     """Raise for empty child type"""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.empty_diff_child())
+            message or args_config_error_conf.empty_diff_child()
+        )
 
-
-class DiffRepoTypeError(base_models_exception.DiffArgsError):
+class DiffRepoTypeError(base_models_exception.DiffArgsError, TypeError):
     """Raise for invalid src_repo type"""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.invalid_diff_repo())
+            message or args_config_error_conf.invalid_diff_repo()
+        )
 
-
-class DiffRepoValueError(base_models_exception.DiffArgsError):
+class DiffRepoValueError(base_models_exception.DiffArgsError, ValueError):
     """Raise for empty src_repo type"""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.empty_diff_repo())
+            message or args_config_error_conf.empty_diff_repo()
+        )
 
-
-class ImmutableDiffArgsError(base_models_exception.DiffArgsError):
+class ImmutableDiffArgsError(base_models_exception.DiffArgsError, TypeError):
     """Raise for any attempt to add or set attributes on InfoArgs."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.immutable_diff_args())
+            message or args_config_error_conf.immutable_diff_args()
+        )
 
-
-class CatInFileTypeError(base_models_exception.CatArgsError):
+class CatInFileTypeError(base_models_exception.CatArgsError, TypeError):
     """Raise for invalid input filename type."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.invalid_cat_infile())
+            message or args_config_error_conf.invalid_cat_infile()
+        )
 
-
-class CatOutFileTypeError(base_models_exception.CatArgsError):
+class CatOutFileTypeError(base_models_exception.CatArgsError, TypeError):
     """Raise for invalid output filename type."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.invalid_cat_outfile())
+            message or args_config_error_conf.invalid_cat_outfile()
+        )
 
-
-class CatVersionTypeError(base_models_exception.CatArgsError):
+class CatVersionTypeError(base_models_exception.CatArgsError, TypeError):
     """Raise for invalid version type."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.invalid_cat_version())
+            message or args_config_error_conf.invalid_cat_version()
+        )
 
-
-class CatInFileValueError(base_models_exception.CatArgsError):
+class CatInFileValueError(base_models_exception.CatArgsError, ValueError):
     """Raise for empty input filename."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.empty_cat_infile())
+            message or args_config_error_conf.empty_cat_infile()
+        )
 
-
-class CatOutFileValueError(base_models_exception.CatArgsError):
+class CatOutFileValueError(base_models_exception.CatArgsError, ValueError):
     """Raise for empty output filename."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.empty_cat_outfile())
+            message or args_config_error_conf.empty_cat_outfile()
+        )
 
-
-class CatOutFileDirError(base_models_exception.CatArgsError):
+class CatOutFileDirError(base_models_exception.CatArgsError, TypeError):
     """Raise for output filename path being a directory."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.outfile_cat_dir_error())
+            message or args_config_error_conf.outfile_cat_dir_error()
+        )
 
-
-class CatVersionValueError(base_models_exception.CatArgsError):
+class CatVersionValueError(base_models_exception.CatArgsError, ValueError):
     """Raise for empty version argument."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.empty_cat_version())
+            message or args_config_error_conf.empty_cat_version()
+        )
 
-
-class CatRepoTypeError(base_models_exception.CatArgsError):
+class CatRepoTypeError(base_models_exception.CatArgsError, TypeError):
     """Raise for invalid src_repo type."""
     def __init__(self, message=None):
         super().__init__(
-            message or args_config_error_conf.invalid_cat_repo())
+            message or args_config_error_conf.invalid_cat_repo()
+        )
 
-
-class CatRepoValueError(base_models_exception.CatArgsError):
+class CatRepoValueError(base_models_exception.CatArgsError, ValueError):
     """Raise for empty src_repo."""
     def __init__(self, message=None):
         super().__init__(
             message or args_config_error_conf.empty_cat_repo())
 
 
-class ImmutableCatArgsError(base_models_exception.CatArgsError):
+class ImmutableCatArgsError(base_models_exception.CatArgsError, TypeError):
     """Raise for any attempt to add or set attributes on InfoArgs."""
     def __init__(self, message=None):
         super().__init__(
             message or args_config_error_conf.immutable_cat_args())
 
 
-class GetTimelineArgTypeError(base_models_exception.GetTimelineArgError):
+class GetTimelineArgTypeError(
+        base_models_exception.GetTimelineArgError, TypeError):
     """Raise for invalid src_repo type."""
     def __init__(self, message=None):
         super().__init__(
             message or args_config_error_conf.missing_user())
 
 
-class GetTimelineArgValueError(base_models_exception.GetTimelineArgError):
+class GetTimelineArgValueError(
+        base_models_exception.GetTimelineArgError, ValueError):
     """Raise for empty src_repo type."""
     def __init__(self, message=None):
         super().__init__(
             message or args_config_error_conf.missing_user())
 
 
-class ImmutableGetTimelineArgError(base_models_exception.GetTimelineArgError):
+class ImmutableGetTimelineArgError(
+        base_models_exception.GetTimelineArgError, TypeError):
     """Raise for any attempt to add or set attributes on GetTimelineArg."""
     def __init__(self, message=None):
         super().__init__(
