01b911d5405330aee18599bf23016b74fa1ff4b8
mesotron.dev <mail@mesotron.dev>
2025-05-03 01:09:09 +0000

    minor updates, linting
    FossilOrigin-Name: f57a9e7c78e7c87322666a25111317d406432800a1f57fc50816a9827025c3b8

--- quarryforge/config/models_conf.py
+++ quarryforge/config/models_conf.py
@@ -9,6 +9,16 @@ class ConfigArgs(Valid):
     """Configuration Arguments
 
     The slots for a Repo Config.
+
+    Args:
+        user (str): Username.
+        emai (str)l: User email.
+        src_repo (Path): Path to the source repository.
+        update_repo (Path): Path to the updated repository.
+        update_dir (Path): Path to the updated project direcory.
+        template (Path): Path for the source repository template.
+        project_name (str): Name of the project.
+        project_desc (str): Description of the project.
     """
     USER = 'user'
     EMAIL = 'email'
@@ -72,5 +82,16 @@ class ConfigCat(Valid):
     """
     FILENAME = 'filename'
     OUTFILE = 'outfile'
-    VERSION = InfoConfig.VERSION.value
+    VERSION = ConfigInfo.VERSION.value
     SRC_REPO = ConfigArgs.SRC_REPO.value
+
+
+class ConfigTimeline(Valid):
+    """Timeline Config
+
+    Defines the slots for a TimelineArg
+
+    Attributes:
+        src: (Path): pathlib.Path location of the fossil repo to reconstruct.
+    """
+    SRC_REPO = 'src_repo'
