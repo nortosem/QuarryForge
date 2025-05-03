2d7156aa1b7134b0d5eafacbfa171accf6866cb0
mesotron.dev <mail@mesotron.dev>
2025-05-03 01:09:36 +0000

    linted
    FossilOrigin-Name: f1b2f7e51ccd20b11b53d5484c05521b34c5439dd78974f9adf916b5f6e125dd

--- quarryforge/exceptions/model_exception/base_models_exception.py
+++ quarryforge/exceptions/model_exception/base_models_exception.py
@@ -13,7 +13,7 @@ class ModelError(QuarryForgeException):
     pass
 
 
-class RepoConfigError(ModelException):
+class RepoConfigError(ModelError):
     """RepoConfig Error
 
     Base exception for the RepoConfig class.
@@ -21,7 +21,7 @@ class RepoConfigError(ModelException):
     pass
 
 
-class InfoArgsError(ModelException):
+class InfoArgsError(ModelError):
     """InfoArgs Error
 
     Base exception for the InfoArgs class.
@@ -29,7 +29,7 @@ class InfoArgsError(ModelException):
     pass
 
 
-class DiffArgsError(ModelException):
+class DiffArgsError(ModelError):
     """DiffArgs Error
 
     Base exception for the DiffArgs class.
@@ -37,7 +37,7 @@ class DiffArgsError(ModelException):
     pass
 
 
-class CatArgsError(ModelException):
+class CatArgsError(ModelError):
     """CatArgs Error
 
     Base exception for the CatArgs class.
@@ -45,7 +45,7 @@ class CatArgsError(ModelException):
     pass
 
 
-class GetTimelineError(ModelException):
+class GetTimelineError(ModelError):
     """GetTimeline Error
 
     Base exception for the GetTimelineArg class.
@@ -53,7 +53,7 @@ class GetTimelineError(ModelException):
     pass
 
 
-class CommitError(ModelException):
+class CommitError(ModelError):
     """Commit Error
 
     Base exception for the Commit class.
@@ -61,12 +61,11 @@ class CommitError(ModelException):
     pass
 
 
-class TimelineError(ModelsException):
+class TimelineError(ModelError):
     """Timeline Parsing Error
 
     Raised when there is an error parsing the output of the `fossil timeline`
     command.  This might occur if the output format is unexpected or if
     there are issues extracting data from the timeline text.
     """
-    def __init__(self, message: str = "Error parsing fossil timeline output"):
-        super().__init__(message)
+    pass
