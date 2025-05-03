0eba0301ff59a15082457b771635b3931eed0e14
mesotron.dev <mail@mesotron.dev>
2025-05-03 01:13:05 +0000

    updated commit exception conf
    FossilOrigin-Name: d5e944653070e73aeb4cd65d49be5cd5e9e0d657f986313eec142fb7e82e7867

---
+++ quarryforge/config/exception_conf/models_exception_conf/commit_error_conf.py
@@ -0,0 +1,74 @@
+"""Commit Error Configuration
+
+
+"""
+from quarryforge.config.exception_conf.fault_builder import Required
+#from quarryforge.config.exception_conf.fault_builder import Immutable
+from quarryforge.config.models_conf import ConfigCommit
+#from quarryforge.config.root import ModelNames as Model
+
+from typing import List
+
+def invalid_commit_hash():
+    """Invalid commit uuid argument"""
+    return Required.field_type(ConfigCommit.HASH, str)
+
+
+def empty_commit_hash():
+    """Commit uuid/hash cannot be empty"""
+    return Required.field_empty(ConfigCommit.HASH, str)
+
+
+def invalid_commit_date():
+    """Invalid commit date argument"""
+    return Required.field_type(ConfigCommit.DATE, str)
+
+
+def empty_commit_date():
+    """Commit date cannot be empty"""
+    return Required.field_empty(ConfigCommit.DATE, str)
+
+
+def invalid_commit_author():
+    """Invalid commit author argument"""
+    return Required.field_type(ConfigCommit.AUTHOR, str)
+
+
+def empty_commit_author():
+    """Commit author cannot be empty"""
+    return Required.field_empty(ConfigCommit.AUTHOR, str)
+
+
+def invalid_commit_comment():
+    """Invalid commit comment argument"""
+    return Required.field_type(ConfigCommit.COMMENT, str)
+
+
+def empty_commit_comment():
+    """Commit comment cannot be empty"""
+    return Required.field_empty(ConfigCommit.COMMENT, str)
+
+
+def invalid_commit_branch():
+    """Invalid commit branch argument"""
+    return Required.field_type(ConfigCommit.BRANCH, str)
+
+
+def empty_commit_branch():
+    """Commit branch cannot be empty"""
+    return Required.field_empty(ConfigCommit.BRANCH, str)
+
+
+def invalid_commit_tags():
+    """Invalid commit tags argument"""
+    return Required.field_type(ConfigCommit.TAGS, List)
+
+
+def invalid_commit_tag_type():
+    """Invalid tag in tags argument"""
+    return Required.field_type(ConfigCommit.TAGS, List[str])
+
+
+def empty_commit_tags():
+    """Tag cannot be empty"""
+    return Required.field_empty(ConfigCommit.TAGS, List[str])
