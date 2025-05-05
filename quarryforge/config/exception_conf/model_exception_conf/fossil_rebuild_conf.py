abb57f2409c2d37f4172d71a40194a6e0e6919ea
mesotron.dev <mail@mesotron.dev>
2025-05-05 21:56:25 +0000

    rename + refactor
    FossilOrigin-Name: 7e8b8165173ed6d23473fa01f0d657d7c0546bae7829de70f33e3d465500dcf0

--- quarryforge/config/exception_conf/model_exception_conf/fossil_rebuild_conf.py
+++ quarryforge/config/exception_conf/model_exception_conf/fossil_rebuild_conf.py
@@ -3,77 +3,86 @@
 
 """
 from pathlib import Path
-from quarryforge.config.exception_conf.fault_builder import Immutable
-from quarryforge.config.exception_conf.fault_builder import PathMessage
-from quarryforge.config.exception_conf.fault_builder import Required
-from quarryforge.config.models_conf import ConfigArgs as Arg
+from quarryforge.config import model_config
+from quarryforge.config.exception_conf import message as Message
 from quarryforge.config.root import ModelNames as Model
 
 
 def empty_user() -> str:
     """The user field is empty message."""
-    return Required.field_empty(Arg.USER, str)
+    return Required.field_empty(model_config.ConfigFossilRebuild.USER, str)
 
 
 def empty_email() -> str:
     """The email field is empty message."""
-    return Required.field_empty(Arg.EMAIL, str)
+    return Required.field_empty(model_config.ConfigFossilRebuild.EMAIL, str)
 
 
 def empty_src() -> str:
     """The src_repo field is empty message."""
-    return Required.field_empty(Arg.SRC_REPO, Path)
+    return Required.field_empty(
+        model_config.ConfigFossilRebuild.SRC_REPO, Path)
 
 
 def empty_update() -> str:
     """The update_repo field is empty message."""
-    return Required.field_empty(Arg.UPDATE_REPO, Path)
+    return Required.field_empty(
+        model_config.ConfigFossilRebuild.UPDATE_REPO, Path)
 
 
 def empty_update_dir() -> str:
     """The update_dir field is empty message."""
-    return Required.field_empty(Arg.UPDATE_REPO, Path)
+    return Required.field_empty(
+        model_config.ConfigFossilRebuild.UPDATE_REPO, Path)
 
 
 def empty_template() -> str:
     """The template field is empty message."""
-    return Required.field_empty(Arg.TEMPLATE, Path)
+    return Required.field_empty(
+        model_config.ConfigFossilRebuild.TEMPLATE, Path)
 
 
 def empty_project_name() -> str:
     """The project_name field is empty message."""
-    return Required.field_empty(Arg.PROJECT_NAME, str)
+    return Required.field_empty(
+        model_config.ConfigFossilRebuild.PROJECT_NAME, str)
 
 
 def empty_project_desc() -> str:
     """The project_desc field is empty message."""
-    return Required.field_empty(Arg.PROJECT_DESC, str)
+    return Required.field_empty(
+        model_config.ConfigFossilRebuild.PROJECT_DESC, str)
 
 
 def invalid_user() -> str:
     """The user field is invalid message."""
-    return Required.field_type(Arg.USER, str)
+    return Required.field_type(model_config.ConfigFossilRebuild.USER, str)
 
 
 def invalid_email() -> str:
     """The email field is invalid message."""
-    return Required.field_type(Arg.EMAIL, str)
+    return Required.field_type(model_config.ConfigFossilRebuild.EMAIL, str)
 
 
 def invalid_src() -> str:
     """The src_repo field is invalid message."""
-    return ' '.join([Required.field_type(Arg.SRC_REPO, str),
-        PathMessage.not_a_path(Arg.SRC_REPO)])
+    return ' '.join([
+        Required.field_type(model_config.ConfigFossilRebuild.SRC_REPO, str),
+        PathMessage.not_a_path(model_config.ConfigFossilRebuild.SRC_REPO)
+    ])
 
 def invalid_update() -> str:
     """The update_repo field is invalid message."""
-    return ' '.join([Required.field_type(Arg.UPDATE_REPO, str),
-                     PathMessage.not_a_path(Arg.UPDATE_REPO)])
+    return ' '.join([
+        Required.field_type(model_config.ConfigFossilRebuild.UPDATE_REPO, str),
+        PathMessage.not_a_path(model_config.ConfigFossilRebuild.UPDATE_REPO)
+    ])
 
 
 def invalid_update_dir() -> str:
     """The update_dir field is invalid message."""
-    return Required.field_type(Arg.UPDATE_DIR, Path)
+    return Required.field_type(
+        model_config.ConfigFossilRebuild.UPDATE_DIR, Path)
 
 
 def immutable_config() -> str:
