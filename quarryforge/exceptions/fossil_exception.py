c6c947cf05e1721da0d57ddf39b22fd42ae662f1
mesotron.dev <mail@mesotron.dev>
2025-05-10 09:54:59 +0000

    templates for fossil module exceptions completed
    FossilOrigin-Name: d44250832fea834322e8ebc28c273b9a6866a11bb10ed7f5914757fefc8f24a1

--- quarryforge/exceptions/fossil_exception.py
+++ quarryforge/exceptions/fossil_exception.py
@@ -128,3 +128,78 @@ class FossilCatError(FossilError):
             details=details,
             user_message=user_message
         )
+
+
+class FossilBranchError(FossilError):
+    """
+
+    Raised when a fossil command executed via subprocess returns a non-zero
+    exit code, indicating an error in the command's execution.  Wraps the
+    subprocess.CalledProcessError with a quarryforge specific exception.
+    """
+    CODE = Message.Default.ERROR.value
+
+    def __init__(self,
+                 message: Optional[str] = None,
+                 code: Optional[str] = None,
+                 details: Optional[Dict] = None,
+                 user_message: Optional[str] = None):
+
+        effective_code = code if code is not None else self.CODE
+
+        super().__init__(
+            message=message,
+            code=effective_code,
+            details=details,
+            user_message=user_message
+        )
+
+
+class FossiAddError(FossilError):
+    """
+
+    Raised when a fossil command executed via subprocess returns a non-zero
+    exit code, indicating an error in the command's execution.  Wraps the
+    subprocess.CalledProcessError with a quarryforge specific exception.
+    """
+    CODE = Message.Default.ERROR.value
+
+    def __init__(self,
+                 message: Optional[str] = None,
+                 code: Optional[str] = None,
+                 details: Optional[Dict] = None,
+                 user_message: Optional[str] = None):
+
+        effective_code = code if code is not None else self.CODE
+
+        super().__init__(
+            message=message,
+            code=effective_code,
+            details=details,
+            user_message=user_message
+        )
+
+
+class FossilCommitError(FossilError):
+    """
+
+    Raised when a fossil command executed via subprocess returns a non-zero
+    exit code, indicating an error in the command's execution.  Wraps the
+    subprocess.CalledProcessError with a quarryforge specific exception.
+    """
+    CODE = Message.Default.ERROR.value
+
+    def __init__(self,
+                 message: Optional[str] = None,
+                 code: Optional[str] = None,
+                 details: Optional[Dict] = None,
+                 user_message: Optional[str] = None):
+
+        effective_code = code if code is not None else self.CODE
+
+        super().__init__(
+            message=message,
+            code=effective_code,
+            details=details,
+            user_message=user_message
+        )
