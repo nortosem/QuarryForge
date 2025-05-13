085706963e66574d23c1e849ed5067a723cf5e52
mesotron.dev <mail@mesotron.dev>
2025-05-13 23:23:09 +0000

    update imports & default error code string
    FossilOrigin-Name: b488abc41c9c2f38c798c701bbab3139b6a81ebf90e070948b7c783559737c7a

--- quarryforge/exceptions/model_exception.py
+++ quarryforge/exceptions/model_exception.py
@@ -5,7 +5,7 @@
 from typing import Dict, Optional
 
 from quarryforge.config import root as get
-from quarryforge.config.exception_conf import message as Message
+from quarryforge.config.exception_conf import model_exception_config as code
 from quarryforge.exceptions import base_exception
 
 
@@ -14,30 +14,8 @@ class FossilRepoError(base_exception.ModelError):
 
     Base exception for the FossilRepo class.
     """
-    CODE = get.ModelNames.FOSSIL_REPO.value + Message.Default.ERROR.value
-
-    def __init__(self,
-                 message: Optional[str] = None,
-                 code: Optional[str] = None,
-                 details: Optional[Dict] = None,
-                 user_message: Optional[str] = None):
-
-        effective_code = code if code is not None else self.CODE
-
-        super().__init__(
-            message=message,
-            code=effective_code,
-            details=details,
-            user_message=user_message
-        )
-
-
-class FossilSetupError(base_exception.ModelError):
-    """
-
-
-    """
-    CODE = get.TopModules.MODEL.value + Message.Default.ERROR.value
+    CODE = code.ModelErrorContext.default_error(
+        code.ModelErrorContext.FOSSIL_REPO)
 
     def __init__(self,
                  message: Optional[str] = None,
@@ -60,7 +38,8 @@ class CommitError(base_exception.ModelError):
 
     Base exception for the Commit class.
     """
-    CODE = get.TopModules.MODEL.value + Message.Default.ERROR.value
+    CODE = code.ModelErrorContext.default_error(
+        code.ModelErrorContext.COMMIT)
 
     def __init__(self,
                  message: Optional[str] = None,
@@ -85,7 +64,8 @@ class TimelineError(base_exception.ModelError):
     command.  This might occur if the output format is unexpected or if
     there are issues extracting data from the timeline text.
     """
-    CODE = get.TopModules.MODEL.value + Message.Default.ERROR.value
+    CODE = code.ModelErrorContext.default_error(
+        code.ModelErrorContext.TIMELINE)
 
     def __init__(self,
                  message: Optional[str] = None,
