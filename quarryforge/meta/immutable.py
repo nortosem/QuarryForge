99bc789173ef44b15e5dc19df4837e3cb2fee277
mesotron.dev <mail@mesotron.dev>
2025-05-16 17:09:27 +0000

    added namespace metaclass, minor fixes, docs & exceptions todo
    FossilOrigin-Name: 0df5fe75368277db49a9982cde52603c90af2a7309281be66680b5fd03f81b74

--- quarryforge/meta/immutable.py
+++ quarryforge/meta/immutable.py
@@ -1,4 +1,4 @@
-"""Immutable Metaclass Module
+"""Immutable class Module
 
 
 """
@@ -8,16 +8,50 @@ from typing import Any, Optional, Dict, List, Tuple
 from quarryforge.config import root
 
 
-class Immutable(type):
+class ImmutableMetaClass(type):
     """Immutable MetaClass
 
-    Metaclass to prevent setting or deleting attributes on the class itself.
-    This allows utility classes with no attributes and ensure that immutable
-    class instances remain immutable on the class itself prior to
-    instantiation.
+    Metaclass to prevent setting or deleting attributes on the class itself
+    after initial class creation.
     """
     def __setattr__(cls, name: str, value: Any) -> None:
-        raise Exception('Immutable')
+        raise AttributeError('Immutable')
 
     def __delattr__(cls, name: str) -> None:
-        raise Exception('Immutable')
+        raise AttributeError('Immutable')
+
+
+class Namespace(ImmutableMetaClass):
+    """Namespace
+
+    Prevent function namespace classes from instantiation.
+    """
+    def __new__(
+            mcs,
+            name: str,
+            bases: Tuple[type, ...],
+            attrs: Dict[str, Any]
+    ):
+        """"""
+        def _uninstantiable(self, *args, **kwargs):
+            raise TypeError('Class has no instances.')
+
+        attrs['__init__'] = _uninstantiable
+        attrs['__slots__'] = ()
+
+        cls = super().__new__(mcs, name, bases, attrs)
+
+        return cls
+
+
+class ImmutableInstance:
+    """ImmutableInstance
+
+    A mixin class to enforce immutability for instances.
+    """
+    def __setattr__(cls, name: str, value: Any) -> None:
+        raise TypeError('Immutable')
+
+    def __delattr__(cls, name: str) -> None:
+        raise TypeError('Immutable')
+
