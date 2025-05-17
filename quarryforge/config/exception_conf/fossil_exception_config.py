860d29e1018a16cda6170c4feea19cff326a8eff
mesotron.dev <mail@mesotron.dev>
2025-05-17 23:37:47 +0000

    linted
    FossilOrigin-Name: 3e7ca63964dcad70fc7cb7ebfe97eb570ea7e61d8ae4b74aaa687be6034d71d0

--- quarryforge/config/exception_conf/fossil_exception_config.py
+++ quarryforge/config/exception_conf/fossil_exception_config.py
@@ -9,11 +9,11 @@ from quarryforge.meta import immutable
 
 class ConfigFossilError(metaclass=immutable.Namespace):
     """Define the fossil error field configuration"""
-    CMD = 'cmd'
-    RETURN_CODE = 'returncode'
-    STDOUT = 'stdout'
-    STDERR = 'stderr'
-    TIMEOUT = 'timeout'
+    CMD: str = 'cmd'
+    RETURN_CODE: str = 'returncode'
+    STDOUT: str = 'stdout'
+    STDERR: str = 'stderr'
+    TIMEOUT: str = 'timeout'
 
 
 class FossilErrorContext(metaclass=immutable.Namespace):
@@ -21,19 +21,19 @@ class FossilErrorContext(metaclass=immutable.Namespace):
 
 
     """
-    FOSSIL_PROCESS = fossil_config.ConfigProcess.path()
-    FOSSIL_TIMEOUT = fossil_config.ConfigTimeout.path()
-    FOSSIL_TIMELINE = fossil_config.ConfigTimeline.path()
-    FOSSIL_SETUP = fossil_config.ConfigSetup.path()
-    FOSSIL_INFO = fossil_config.ConfigInfo.path()
-    FOSSIL_DIFF = fossil_config.ConfigDiff.path()
-    FOSSIL_CAT = fossil_config.ConfigCat.path()
-    FOSSIL_BRANCH = fossil_config.ConfigBranch.path()
-    FOSSIL_ADD = fossil_config.ConfigAdd.path()
-    FOSSIL_COMMIT = fossil_config.ConfigCommit.path()
+    FOSSIL_PROCESS: str = fossil_config.ConfigProcess.path()
+    FOSSIL_TIMEOUT: str = fossil_config.ConfigTimeout.path()
+    FOSSIL_TIMELINE: str = fossil_config.ConfigTimeline.path()
+    FOSSIL_SETUP: str = fossil_config.ConfigSetup.path()
+    FOSSIL_INFO: str = fossil_config.ConfigInfo.path()
+    FOSSIL_DIFF: str = fossil_config.ConfigDiff.path()
+    FOSSIL_CAT: str = fossil_config.ConfigCat.path()
+    FOSSIL_BRANCH: str = fossil_config.ConfigBranch.path()
+    FOSSIL_ADD: str = fossil_config.ConfigAdd.path()
+    FOSSIL_COMMIT: str = fossil_config.ConfigCommit.path()
 
-    @classmethod
-    def default_error(cls, context: str):
+    @staticmethod
+    def default_error(context: str):
         """Default Exception Context Code"""
         if not isinstance(context, str):
             raise TypeError('todo')
