d8aefccf73c37aef003e4953493b4c7ae986d455
mesotron.dev <mail@mesotron.dev>
2025-05-20 23:04:29 +0000

    fixed default messages
    FossilOrigin-Name: 81ec53861fade0f3a34c3222b590747963843fbdb860907758ee621b53f0bb38

--- quarryforge/config/exception_conf/base_exception_config.py
+++ quarryforge/config/exception_conf/base_exception_config.py
@@ -63,16 +63,18 @@ class DefaultCode(metaclass=immutable.Namespace):
 class DefaultMessage(metaclass=immutable.Namespace):
     """The default base_exception error messages."""
     unexpected_error = (
-        'An unexpected error occurred within the ')
+        'An unexpected error occurred within the')
     quarryforge_error = (
-        f'{root.PACKAGE.name} package.')
+        f'{unexpected_error} {root.PACKAGE.name} package.')
     model_error = (
-        f'{root.PACKAGE.name}.{root.Module.model} module.')
+        f'{unexpected_error}  {root.PACKAGE.name}.{root.Module.model} module.')
     fossil_error = (
-        f'{root.PACKAGE.name}.{root.Module.model} module.')
+        f'{unexpected_error} {root.PACKAGE.name}.{root.Module.model} module.')
     main_error = (
-        f'{root.PACKAGE.name}.{root.Module.model} module.')
+        f'{unexpected_error} {root.PACKAGE.name}.{root.Module.model} module.')
     meta_error = (
+        f'{unexpected_error} '
         f'{root.PACKAGE.name}.{root.SubPackage.meta} subpackage.')
     util_error = (
+        f'{unexpected_error} '
         f'{root.PACKAGE.name}.{root.SubPackage.util} subpackage.')
