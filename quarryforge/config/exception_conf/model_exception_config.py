52e79be9cfc6a1298fa32f88ee775f9ec4abd3a1
mesotron.dev <mail@mesotron.dev>
2025-05-20 23:38:47 +0000

    added types and __all__
    FossilOrigin-Name: 3169f6383a46e42c050f16ac4be2664453e9253f8ec60d2e6dcb9efe93109524

--- quarryforge/config/exception_conf/model_exception_config.py
+++ quarryforge/config/exception_conf/model_exception_config.py
@@ -20,37 +20,43 @@ from quarryforge.config.exception_conf import exception_config as msg
 from quarryforge.meta import immutable
 
 
+__all__: List[str] = [
+    'DefaultCode',
+    'DefaultMessage',
+]
+
+
 class ModelErrorContext(metaclass=immutable.Namespace):
     """Context for Model Errors"""
-    FOSSIL_COMMIT = model_config.FOSSIL_COMMIT.path()
-    FOSSIL_REPO = model_config.FOSSIL_REPO.path()
-    FOSSIL_TIMELINE = model_config.FOSSIL_TIMELINE.path()
+    FOSSIL_COMMIT: str = model_config.FOSSIL_COMMIT.path()
+    FOSSIL_REPO: str = model_config.FOSSIL_REPO.path()
+    FOSSIL_TIMELINE: str = model_config.FOSSIL_TIMELINE.path()
 
     @staticmethod
-    def default_error(model: str):
+    def default_error(context: str):
         """Default Exception Context Code"""
-        return f'{model}.{msg.Default.ERROR}'
+        return f'{context}.{msg.Default.ERROR}'
 
 
 class DefaultCode(metaclass=immutable.Namespace):
     """The Default Exception Codes for Model Exceptions."""
-    fossil_repo = ModelErrorContext.default_error(
+    fossil_repo: str = ModelErrorContext.default_error(
         ModelErrorContext.FOSSIL_REPO)
-    fossil_commit = ModelErrorContext.default_error(
+    fossil_commit: str = ModelErrorContext.default_error(
         ModelErrorContext.FOSSIL_COMMIT)
-    fossil_timeline = ModelErrorContext.default_error(
+    fossil_timeline: str = ModelErrorContext.default_error(
         ModelErrorContext.FOSSIL_TIMELINE)
 
 
 class DefaultMessage(metaclass=immutable.Namespace):
     """The Default Exception Messages for Model Exceptions."""
-    fossil_repo = (
+    fossil_repo: str = (
         f'{base_exception_config.unexpected_error} '
         f'{root.PACKAGE.name}.{root.Module.model}.{root.Model.fossil_repo}')
-    fossil_commit = (
+    fossil_commit: str = (
         f'{base_exception_config.unexpected_error} '
         f'{root.PACKAGE.name}.{root.Module.model}.{root.Model.fossil_commit}')
-    fossil_timeline = (
+    fossil_timeline: str = (
         f'{base_exception_config.unexpected_error} '
         f'{root.PACKAGE.name}.{root.Module.model}.'
         f'{root.Model.fossil_timeline}')
@@ -61,22 +67,22 @@ class FossilRepoContext(metaclass=immutable.Namespace):
 
     Messages for FossilRepo.
     """
-    EMPTY_STR_MSG = ''
+    EMPTY_STR_MSG: str = ''
   #  msg.Argument.missing(
   #      ConfigFossilRepo.FILE, (msg.Default.SPC).join(
   #          msg.ErrorKind.STR,
   #          msg.Default.BAR,
   #          msg.ErrorKind.PATH)
   #      )
-  #  EMPTY_STR_USER_MSG = msg.Required.field_empty(
+  #  EMPTY_STR_USER_MSG: str = msg.Required.field_empty(
   #      ConfigFossilRepo.file(), str
   #  )
-  #  INVALID = (msg.Default.NL).join(
+  #  INVALID: str = (msg.Default.NL).join(
   #      [msg.Argument.invalid(
   #          ConfigFossilRepo.file()),
   #       msg.Required.field_type(ConfigFossilRepo.file())]
   #  )
-    NOT_EXIST = ()
-    NOT_FILE = ()
-    NOT_READ = ()
-    DIR_WRITE = ()
+    NOT_EXIST: str = ()
+    NOT_FILE: str = ()
+    NOT_READ: str = ()
+    DIR_WRITE: str = ()
