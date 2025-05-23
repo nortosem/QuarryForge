01f00398914f2fb3bda9ac720eb737d280a86737
mesotron.dev <mail@mesotron.dev>
2025-05-23 12:37:21 +0000

    declare valid error data structure.
    FossilOrigin-Name: 351789ed14ee92a35f134161008574400962307a67d92da9763bf5819b5d88f9

--- quarryforge/config/exception_conf/exception_data.py
+++ quarryforge/config/exception_conf/exception_data.py
@@ -2,8 +2,9 @@
 
 
 """
-from typing import Any, Dict, NamedTuple
+from typing import Any, Dict, NamedTuple, Optional
 
+from quarryforge.meta import immutable as _
 
 __all__: list = ['ERROR_FIELD', 'BUILDER_FIELD', 'ValidErrorData']
 
@@ -37,12 +38,25 @@ BUILDER_FIELD: ConfigBuilder = ConfigBuilder()
 """Global constant for BuildError data fields."""
 
 
-class ValidErrorData(NamedTuple):
+class ValidErrorData(_.ImmutableInstance, metaclass=_.ImmutableMetaClass):
     """Data container for exception information"""
-    code: str
-    message: str
-    user_message: str
-    details: Dict[str, Any]
+    __slots__ = ERROR_FIELD._fields[:-1]
+
+    code: Optional[str]
+    message: Optional[str]
+    user_message: Optional[str]
+    details: Optional[Dict[str, Any]]
+
+    def __init__(self,
+                 code: Optional[str] = None,
+                 message: Optional[str] = None,
+                 user_message: Optional[str] = None,
+                 details: Optional[Dict[str, Any]] = None):
+        object.__setattr__(self, ERROR_FIELD.code, code)
+        object.__setattr__(self, ERROR_FIELD.message, message)
+        object.__setattr__(self, ERROR_FIELD.user_message, user_message)
+        object.__setattr__(self, ERROR_FIELD.details, details)
+
 
     def to_exception(self) -> Dict[str, Any]:
         """Assemble to convert keyword args for a QuarryForge exception."""
