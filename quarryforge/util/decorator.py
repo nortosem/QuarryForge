d62c356864bc001bd80467bf1ec37b3382139e0d
mesotron.dev <mail@mesotron.dev>
2025-05-27 17:22:29 +0000

    refactor decorator for str parameters. No functions expext more than 10 args.
    FossilOrigin-Name: 72d5a9672064a85ba3f924e663d2d9e5e05f5be03ab0ce8073c97aff3be78e53

--- quarryforge/util/decorator.py
+++ quarryforge/util/decorator.py
@@ -8,55 +8,52 @@ from typing import Callable, List, ParamSpec, TypeVar
 from quarryforge.config.exception_conf import exception_config as config
 
 
-__all__: List = ['validate_str_parameter']
+__all__: List = ['validate_str_parameters']
 
 
 _PARAM = ParamSpec('_PARAM')
 _FUNC = TypeVar('_FUNC')
 
 
-def validate_str_parameter(
+def validate_str_parameters(
     method: Callable[_PARAM, _FUNC]
 ) -> Callable[_PARAM, _FUNC]:
     """Decorator for staticmethods of namespaces.
 
-    The decorator validates a staticmethod with a single string argument.
+    The decorator validates a staticmethod with a string argument.
 
     Args:
-        method: The staticmethod being decorated.
+        method: The method being decorated.
 
     Returns:
         The wrapped method with validation logic.
 
     Raises:
-        TypeError: If more than one argument is provided.
+        TypeError: If a keyword argument is provided.
         TypeError: If the argument is not a string.
         ValueError: If the argument is an empty string.
     """
     @wraps(method)
     def wrapper(*args: _PARAM.args, **kwargs: _PARAM.kwargs) -> _FUNC:
-        if len(args) != 1:
+        if len(args) > 10:
             raise TypeError(
-                f'Function {method.__name__}: Expected exactly '
-                f'one positional argument value. Got {len(args)} instead.'
+                f'This decorator supports a maximum of ten arguments. '
+                f'Got {len(args)}.'
             )
-
         if kwargs:
             raise TypeError(
                 f'Function {method.__name__}: keyword arguments not supported.'
             )
-
-        parameter = args[0]
-
-        if not isinstance(parameter, str):
-            raise TypeError(
-                f'{parameter!r} {config.DESC_TYPE.must_be}'
-                f' {config.DESC_TYPE.string}'
-            )
-        if not parameter:
-            raise ValueError(
-                f'{parameter!r} {config.DESC_TYPE.must_be}'
-                f' {config.DESC_TYPE.unempty}'
-            )
+        for parameter in args:
+            if not isinstance(parameter, str):
+                raise TypeError(
+                    f'{parameter!r} {config.DESC_TYPE.must_be}'
+                    f' {config.DESC_TYPE.string}'
+                )
+            if not parameter:
+                raise ValueError(
+                    f'{parameter!r} {config.DESC_TYPE.must_be}'
+                    f' {config.DESC_TYPE.unempty}'
+                )
         return method(*args, **kwargs)
     return wrapper
