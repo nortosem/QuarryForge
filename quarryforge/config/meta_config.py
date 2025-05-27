40a5aee93e211c4f80615f7484027384f80feeca
mesotron.dev <mail@mesotron.dev>
2025-05-27 09:33:10 +0000

    adde config for message builder
    FossilOrigin-Name: e0b673893895ff4b02b2a5ff619feb4892504c47e04c77c13ebe94ea8f84085d

--- quarryforge/config/meta_config.py
+++ quarryforge/config/meta_config.py
@@ -12,8 +12,25 @@ class BuildErrorConfig(NamedTuple):
     """The meta.assembler.BuildError configuration.
 
     Define the default slots for all BuildError instances.
+
+    Attributes:
+        error_context:
+        error_code:
+        extra_details:
+        input_value:
     """
     error_context: str = 'error_context'
     error_code: str = 'error_code'
     extra_details: str = 'extra_details'
     input_value: str = 'input_value'
+
+
+class ErrorMessageBuilder(NamedTuple):
+    """The meta.assembler.ErrorMessageBuilder configuration.
+
+    Define the default slots for all BuildErrorMessage instances.
+
+    Attributes:
+        error_types: The error types that messages are built for.
+    """
+    error_types: str = 'error_types'
