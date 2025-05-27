f0fb04863c59562affc5897d575fa65c6ec7de19
mesotron.dev <mail@mesotron.dev>
2025-05-27 09:44:20 +0000

    refactored, linted, type checked
    FossilOrigin-Name: 930d584fd358f97a244f3bd31737d30f865d2c30c72c7db1b930441b95c16526

--- quarryforge/config/exception_conf/exception_config.py
+++ quarryforge/config/exception_conf/exception_config.py
@@ -1,54 +1,41 @@
-"""Fault Builder
+"""Exception Configuration Module
 
 
 """
-from typing import NamedTuple
-
-from quarryforge.meta import immutable
+from typing import List, NamedTuple
 
 
-__all__: list = [
-    'DESC_TYPE', 'ERROR_TYPE', 'ARG',
-    'DEFAULT', 'IMMUTABLE', 'PATH_MSG', 'REQUIRED'
+__all__: List = [
+    'GENERIC_ERROR',
+    'STRING_ERROR',
+    'PATH_ERROR',
 ]
 
 
-class DescType(NamedTuple):
-    """Description strings for errors."""
-    unknown: str = 'Unknown'
-    string: str = 'str'
-    path: str = 'Path'
-    empty: str = 'empty_string'
-    exists: str = 'Path_exists'
-    is_file: str = 'File_Path'
-    is_dir: str = 'Directory_Path'
-    is_readable: str = 'readable_Path'
-    is_writable: str = 'writable_Path'
-    is_executable: str = 'executable_Path'
-    is_list: str = 'List'
-    str_list_content = 'List[str]'
-    non_empty_list_content = 'non-empty_List[str]'
-
-
-DESC_TYPE: DescType = DescType()
-"""Global ..."""
-
-
 class GenericErrorType(NamedTuple):
-    """A collection of constants for generic types of errors."""
+    """The constants codes for generic types of errors."""
     type_error: str = 'TYPE_ERROR'
     unexpected_error: str = 'UNEXPECTED_ERROR'
     value_error: str = 'VALUE_ERROR'
 
 
+GENERIC_ERROR = GenericErrorType()
+"""Global instance for general error types."""
+
+
 class StringErrorType(NamedTuple):
-    """A collection of constants for errors involving strings."""
-    empty: str = 'EMPTY_STRING_ERROR'
-    invalid: str = 'INVALID_STRING_ERROR'
+    """The constant codes for errors involving strings."""
+    type_error: str = 'STRING'
+    empty_error: str = 'EMPTY_STRING'
+    invalid_error: str = 'INVALID_STRING'
+
+
+STRING_ERROR: StringErrorType = StringErrorType()
+"""Global instance for string error types."""
 
 
 class PathErrorType(NamedTuple):
-    """A collection of constants for error involving Path objects."""
+    """The constant codes for errorw involving Path objects."""
     non_path_object: str = 'NON_PATH_OBJECT_ERROR'
     invalid_path_string: str = 'INVALID_PATH_STRING_ERROR'
     resolution: str = 'PATH_RESOLUTION_ERROR'
@@ -61,120 +48,5 @@ class PathErrorType(NamedTuple):
     unexecutable: str = 'PATH_NOT_EXECUTABLE_ERROR'
 
 
-class Argument(NamedTuple):
-    """Argument Messages"""
-    ARG: str = 'argument'
-    INVALID: str = 'Invalid'
-    MISSING: str = 'Missing'
-    TYPE: str = 'type:'
-
-    def invalid(self, name: str):
-        return f'{self.INVALID} {name} {self.ARG}'
-
-    def missing(self, name: str, kind: str) -> str:
-        return f'{self.MISSING} {self.ARG} {name} {self.TYPE} {kind}'
-
-    def type_message(
-        self,
-        arg: str,
-        error: str,
-        context: str,
-        field: str,
-        type_: str,
-    ) -> str:
-        return (
-            f'{error} in context "{context}" for field: {field}. '
-            f'{type_} expected, but got type {type(arg).__name__} instead.'
-        )
-
-ARG: Argument = Argument()
-"""Global ..."""
-
-
-class Default(NamedTuple):
-    """Default message parts"""
-    EMPTY: str = 'EMPTY'
-    ERROR: str = 'DEFAULT_ERROR'
-
-
-DEFAULT: Default = Default()
-"""Global ..."""
-
-
-class Immutable(NamedTuple):
-    """Immutable Message"""
-    MESSAGE: str = 'object is immutable.'
-
-    def error_message(self, name: str):
-        return f'{name} {self.MESSAGE}'
-
-
-IMMUTABLE: Immutable = Immutable()
-"""Global ..."""
-
-
-class PathMessage(NamedTuple):
-    """Filename Message
-
-
-    """
-    NAP: str = 'is not a path.'
-    DNE: str = 'does not exist.'
-    NAF: str = 'is not a file.'
-    NAD: str = 'is not a directory.'
-    READ: str = 'is not readable.'
-    WRITE: str = 'is not writable.'
-    NO_DIR: str = 'cannot write output to a directory.'
-
-    def dir_not_allowed(self, path: str):
-        return f'{path} {self.NO_DIR}'
-
-    def not_a_path(self, path: str) -> str:
-        return f'{path} {self.NAP}'
-
-    def does_not_exist(self, path: str) -> str:
-        return f'{path} {self.DNE}'
-
-    def not_a_file(self, path: str) -> str:
-        return f'{path} {self.NAF}'
-
-    def not_a_directory(self, path: str) -> str:
-        return f'{path} {self.NAD}'
-
-    def no_read_permission(self, path: str) -> str:
-        return f'{path} {self.READ}'
-
-    def no_write_permission(self, path: str) -> str:
-        return f'{path} {self.WRITE}'
-
-
-PATH_MSG: PathMessage = PathMessage()
-"""Global ..."""
-
-
-class Required(NamedTuple):
-    """Field Missing Message
-
-
-    """
-    THE: str = 'The'
-    EMPTY: str = 'field cannot be empty'
-    REQUIRE: str = 'field requires a'
-
-    def field_type(self, field: str, kind: type):
-        return f'{self.THE} {field} {self.REQUIRE} {str(kind)}'
-
-    def field_empty(self, field: str, kind: type):
-        return f'{self.THE} {str(kind)} {field} {self.EMPTY}'
-
-
-REQUIRED: Required = Required()
-"""Global ..."""
-
-
-class ErrorMessageMaker(metaclass=immutable.Namespace):
-    """ErrorMessageMaker creates error messages.
-
-    ErrorTypes and context information determine which messages are produced.
-    """
-
+PATH_ERROR: PathErrorType = PathErrorType()
+"""Global instance for path error types."""
