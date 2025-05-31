1e2708737807cc9caff378051447ae63fdc9bf18
mesotron.dev <mail@mesotron.dev>
2025-05-31 23:48:06 +0000

    stub, main is last on queue.
    FossilOrigin-Name: 9c290ae39f076bcfb59e6ab7f3d581dde59e3e695f2bba357c3cad3b01eb15ac

--- quarryforge/exception/main_exception.py
+++ quarryforge/exception/main_exception.py
@@ -6,15 +6,7 @@ from quarryforge.exception.base_exception import QuarryForgeError
 
 
 class MainError(QuarryForgeError):
-    """Main Error"""
+    """Main Erro
+    #TODO
+    r"""
     pass
-
-
-class ArgumentError(MainError):
-    """Argument Error exception for quarryforge
-
-    Raised when invalid arguments or combinations escape argparse checks.
-    """
-    def __init__(self, message: str = 'A quarryforge argument error occurred'):
-        super().__init__(message)
-        self.message = message
