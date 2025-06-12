1dbcd9da863687ac6b497b96c2404fb5f572b478
mesotron.dev <mail@mesotron.dev>
2025-06-12 06:27:29 +0000

    refactored, fixes, linted, type checked
    FossilOrigin-Name: 0f4ecfbbffabb357c2185682a732c276892db9e67f8b4a084e82a6d5ff26de5c

--- quarryforge/config/exception_conf/base_exception_config.py
+++ quarryforge/config/exception_conf/base_exception_config.py
@@ -55,7 +55,7 @@ class BaseErrorMessageConfig(NamedTuple):
         default_user_message:
             A generic user-friendly message.
     """
-    unexpected_error_suffix: str = config.GENERIC_ERROR.unexpected_error
+    unexpected_error_suffix: str = config.GenericError.UNEXPECTED_ERROR
     unexpected_error_prefix: str = 'An unexpected error occurred within'
     default_module_suffix: str = 'module.'
     default_subpackage_suffix: str = 'subpackage.'
@@ -76,20 +76,14 @@ class BaseErrorBuilder(assembler.ErrorBuilder):
     """
 
     def code(self) -> str:
-        """Generate the complete unique exception code for the error.
-
-        Combines the full path context for the error with an error code.
-
-        Returns:
-            The full error code string for an exception.
-        """
+        """Generate the complete unique exception code for the error."""
         return get_full_error_code(
             self.error_context, self.error_code
         )
 
     def _type_message(self) -> str:
         """Generates a message for type errors."""
-        info = self.info or config.DESC_MSG.unknown
+        info = self.info or config.DescMsg.UNKNOWN
         return (
             f'{self._base_message()} Expected type: {info}. Got '
             f'type {type(self.arg).__name__} with value {self.arg!r} instead.'
@@ -97,7 +91,7 @@ class BaseErrorBuilder(assembler.ErrorBuilder):
 
     def _value_message(self) -> str:
         """Generates a message for value errors."""
-        info = self.info or config.DESC_MSG.unknown
+        info = self.info or config.DescMsg.UNKNOWN
         return (
             f'{self._base_message()} Value {self.arg!r} is invalid. '
             f'Expected value: {info}.'
@@ -109,13 +103,11 @@ class BaseErrorBuilder(assembler.ErrorBuilder):
         Exceptions for invalid states require an action and current_state.
         (e.g. use the extra_details dictionary: extra_details['action'] = ...)
         """
-        action = 'an operation'
-        current_state = f'an {config.DESC_MSG.unknown} state'
-
-        if self.extra_details:
-            action = str(self.extra_details.get('action'))
-            current_state = str(self.extra_details.get('current_state'))
-
+        details = self.extra_details or {}
+        action = details.get('action', 'an operation')
+        current_state = details.get(
+            'current_state', f'an {config.DescMsg.UNKNOWN} state'
+        )
         return (
             f'{self._base_message()} Attempted {action} in {current_state}. '
             f'This state is invalid for this operation.'
@@ -127,16 +119,15 @@ class BaseErrorBuilder(assembler.ErrorBuilder):
         Exceptions for configuration require a configuration value & reason.
         (e.g. use the extra_details dictionary: extra_details['action'] = ...)
         """
-        config_key = 'required configuration'
-        reason = f'with {config.DESC_MSG.unknown} {config.DESC_MSG.reason}'
-
-        if self.extra_details:
-            config_key = str(self.extra_details.get('config_key'))
-            reason = str(self.extra_details.get(config.DESC_MSG.reason))
-
+        details = self.extra_details or {}
+        config_key =  details.get('config_key', 'required configuration')
+        reason = details.get(
+            config.DescMsg.REASON,
+            f'with {config.DescMsg.UNKNOWN} {config.DescMsg.REASON}'
+        )
         return (
             f'{self._base_message()} Missing or invalid configuration for '
-            f'{config_key} {reason}.'
+            f'{config_key}. Reason: {reason}.'
         )
 
     def _dependency_message(self) -> str:
@@ -145,39 +136,42 @@ class BaseErrorBuilder(assembler.ErrorBuilder):
         Exceptions for dependency errors require a dependency and reason.
         (e.g. use the extra_details dictionary: extra_details['action'] = ...)
         """
-        dependency = (
-            f'{config.DESC_MSG.unknown} {config.DESC_MSG.dependency}')
-        reason = f'{config.DESC_MSG.unknown} {config.DESC_MSG.reason}'
-
-        if self.extra_details:
-            dependency = self.extra_details[config.DESC_MSG.dependency]
-            reason = self.extra_details[config.DESC_MSG.reason]
+        details = self.extra_details or {}
+        dependency = details.get(
+            config.DescMsg.DEPENDENCY,
+            f'{config.DescMsg.UNKNOWN} {config.DescMsg.DEPENDENCY}'
+        )
+        reason = details.get(
+            config.DescMsg.REASON,
+            f'{config.DescMsg.UNKNOWN} {config.DescMsg.REASON}'
+        )
         return (
             f'{self._base_message()} `{dependency}` failed due to: {reason}.'
         )
 
     def _not_implemented_message(self) -> str:
         """Generates a message for not implemented errors."""
-        info = self.info or f'{config.DESC_MSG.unknown} feature'
+        info = self.info or f'{config.DescMsg.UNKNOWN} feature'
         return f'{self._base_message()} {info} is not implemented.'
 
     def _unexpected_message(self) -> str:
         """Generates a message for unexpected errors."""
         context = self.error_context.split('.')
+        cfg = base_error_message()
+
         if len(context) == 1 and context[0] == root.Package.NAME:
-            suffix = base_error_message().default_package_suffix
+            suffix = cfg.default_package_suffix
         elif (
-            len(context) == 2 and
+            len(context) > 1 and
             context[0] == root.Package.NAME and
             context[1] in root.SubPackage.__members__
         ):
-            suffix = base_error_message().default_subpackage_suffix
+            suffix = cfg.default_subpackage_suffix
         else:
-            suffix = base_error_message().default_module_suffix
+            suffix = cfg.default_module_suffix
 
         return (
-            f'{base_error_message().unexpected_error_prefix}'
-            f' `{self.error_context}` {suffix}'
+            f'{cfg.unexpected_error_prefix} `{self.error_context}` {suffix}'
         )
 
     def message(self) -> str:
@@ -186,80 +180,75 @@ class BaseErrorBuilder(assembler.ErrorBuilder):
         Returns:
             A string representing the detailed error message.
         """
-        error_message = ''
-
         match self.error_code:
-            case config.GENERIC_ERROR.type_error:
-                error_message = self._type_message()
+            case config.GenericError.TYPE_ERROR:
+                return self._type_message()
 
-            case config.GENERIC_ERROR.value_error:
-                error_message = self._value_message()
+            case config.GenericError.VALUE_ERROR:
+                return self._value_message()
 
-            case config.GENERIC_ERROR.invalid_state:
-                error_message = self._invalid_state_message()
+            case config.GenericError.INVALID_STATE_ERROR:
+                return self._invalid_state_message()
 
-            case config.GENERIC_ERROR.configuration_error:
-                error_message = self._config_message()
+            case config.GenericError.CONFIGURATION_ERROR:
+                return self._config_message()
 
-            case config.GENERIC_ERROR.external_dependency_error:
-                error_message = self._dependency_message()
+            case config.GenericError.EXTERNAL_DEPENDENCY_ERROR:
+                return self._dependency_message()
 
-            case config.GENERIC_ERROR.not_implemented_error:
-                error_message = self._not_implemented_message()
+            case config.GenericError.NOT_IMPLEMENTED_ERROR:
+                return self._not_implemented_message()
 
-            case config.GENERIC_ERROR.unexpected_error:
-                error_message = self._unexpected_message()
+            case config.GenericError.UNEXPECTED_ERROR:
+                return self._unexpected_message()
 
             case _:
-                error_message = (
+                return (
                     f'{self._base_message()} Error code {self.error_code} '
-                    f'unhandled {config.DESC_MSG.unexpected_error}.'
+                    f'unhandled {config.DescMsg.IS_AN_UNEXPECTED_ERROR}.'
                 )
 
-        return error_message
 
     def user_message(self) -> str:
         """Builds a user-friendly error message for base exceptions.
 
-        This method provides user-friendly messages for `GENERIC_ERROR` types,
+        This method provides user-friendly messages for `GenericError` types,
         falling back to a general internal error message.
 
         Returns:
             A string representing the user-friendly error message.
         """
-        error_message = ''
+        cfg = base_error_message()
 
         match self.error_code:
-            case config.GENERIC_ERROR.type_error:
-                error_message = (
+            case config.GenericError.TYPE_ERROR:
+                return (
                     'An input was provided in an incorrect format.'
                 )
 
-            case config.GENERIC_ERROR.value_error:
-                error_message = (
+            case config.GenericError.VALUE_ERROR:
+                return (
                     'An input value is not valid for this operation.'
                 )
 
-            case config.GENERIC_ERROR.invalid_state:
-                error_message = (
+            case config.GenericError.INVALID_STATE_ERROR:
+                return (
                     'An operation attempted in an invalid application state.'
                 )
 
-            case config.GENERIC_ERROR.configuration_error:
-                error_message = 'There is an issue with the configuration.'
+            case config.GenericError.CONFIGURATION_ERROR:
+                return 'There is an issue with the configuration.'
 
-            case config.GENERIC_ERROR.external_dependency_error:
-                error_message = (
+            case config.GenericError.EXTERNAL_DEPENDENCY_ERROR:
+                return (
                     'An required tool or service encountered an issue.'
                 )
 
-            case config.GENERIC_ERROR.not_implemented_error:
-                error_message = 'This feature is not available.'
+            case config.GenericError.NOT_IMPLEMENTED_ERROR:
+                return 'This feature is not available.'
 
-            case config.GENERIC_ERROR.unexpected_error:
-                error_message = base_error_message().default_user_message
+            case config.GenericError.UNEXPECTED_ERROR:
+                return cfg.default_user_message
 
             case _:
-                error_message = base_error_message().default_user_message
-
-        return error_message
+                return cfg.default_user_message
