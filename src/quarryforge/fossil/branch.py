0d80b444bfee57bc0cdb62d719d14223cffbd9de
mesotron.dev <mail@mesotron.dev>
2025-06-30 18:48:02 +0000

    typed updates
    FossilOrigin-Name: 4bf39cbe401a22cec5216b587f2aae2e0e11bd7e101d68230ac11e9f4aea6698

---
+++ src/quarryforge/fossil/branch.py
@@ -0,0 +1,110 @@
+"""Fossil Branch Process
+
+Manage branch info using fossil branch
+"""
+
+import subprocess
+
+from quarryforge import model
+from quarryforge.config import fossil_config as _
+from quarryforge.config.exception_conf import exception_config as ec
+from quarryforge.config.exception_conf import exception_data as e_data
+from quarryforge.config.exception_conf import (
+    fossil_exception_config as fossil_ec,
+)
+from quarryforge.exception import fossil_exception
+from quarryforge.util import fossil_util
+
+
+def list_all(source: model.FossilRepo) -> str:
+    """List all Branches for the source reposiotory.
+
+    Executes the Fossil 'branch list --all' command to list all branches
+    in a given repository.
+
+    Args:
+        source (model.FossilRepo): The repository to list branches from.
+
+    Returns:
+        str: The raw output string containing the list of branches.
+
+    Raises:
+        fossil_exception.FossilProcessError:
+            If the Fossil command fails.
+        fossil_exception.FossilTimeoutError:
+            If the Fossil command times out.
+        fossil_exception.FossilBranchError:
+            If an issue occurs during the branch operation.
+
+    """
+    try:
+        list_process: subprocess.CompletedProcess[bytes] = subprocess.run(
+            fossil_util.ls_branches(source),
+            cwd=source.file.parent,
+            capture_output=True,
+            check=True,
+            timeout=int(_.Fossil.DEFAULT_TIMEOUT),
+        )
+        return list_process.stdout.decode()
+    except subprocess.CalledProcessError as error:
+        process_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_PROCESS,
+            error_code=_.Fossil.PROCESS_ERROR,
+            arg=str(source),
+            extra_details={
+                _.Fossil.CMD: error.cmd,
+                _.Fossil.RETURN_CODE: error.returncode,
+                _.Fossil.OUTPUT: error.stdout,
+                _.Fossil.STDERR: error.stderr,
+            },
+        )
+        process_data = process_builder.data()
+        process_error = fossil_exception.FossilProcessError(
+            **process_data.to_exception()
+        )
+        branch_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_BRANCH,
+            error_code=ec.GenericError.EXTERNAL_DEPENDENCY_ERROR,
+            arg=str(source),
+            extra_details={
+                ec.DescMsg.DEPENDENCY: process_error.details[_.Fossil.CMD],
+                ec.DescMsg.REASON: process_error.details[_.Fossil.STDERR],
+            },
+        )
+        branch_data = branch_builder.data()
+        raise fossil_exception.FossilBranchError(
+            **branch_data.to_exception()
+        ) from process_error
+    except subprocess.TimeoutExpired as error:
+        timeout_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_TIMEOUT,
+            error_code=_.Fossil.TIMEOUT_ERROR,
+            arg=str(source),
+            info=str(error),
+            extra_details={
+                _.Fossil.ARGS: error.args,
+                _.Fossil.CMD: error.cmd,
+                _.Fossil.TIMEOUT: error.timeout,
+                _.Fossil.OUTPUT: error.stdout,
+                _.Fossil.STDERR: error.stderr,
+            },
+        )
+        timeout_data = timeout_builder.data()
+        timeout_error = fossil_exception.FossilTimeoutError(
+            **timeout_data.to_exception()
+        )
+        branch_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_BRANCH,
+            error_code=ec.GenericError.INVALID_STATE_ERROR,
+            arg=str(source),
+            info=timeout_error.details[e_data.builder_config().info],
+            extra_details={
+                _.Fossil.ARGS: timeout_error.details[_.Fossil.ARGS],
+                _.Fossil.CMD: timeout_error.details[_.Fossil.CMD],
+                _.Fossil.TIMEOUT: timeout_error.details[_.Fossil.TIMEOUT],
+            },
+        )
+        branch_data = branch_builder.data()
+        raise fossil_exception.FossilBranchError(
+            **branch_data.to_exception()
+        ) from timeout_error
