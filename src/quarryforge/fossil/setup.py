30c3ad419b75b7b39dac0e82887a78d9b0b947ec
mesotron.dev <mail@mesotron.dev>
2025-06-30 19:46:43 +0000

    linted, typed, refactored
    FossilOrigin-Name: 1ed4c060847c27c5bfe0e6cc4154a7200328014e5ee30ec16f8d35f315b904a1

---
+++ src/quarryforge/fossil/setup.py
@@ -0,0 +1,345 @@
+"""Setup a target repository to store the changed source repository.
+
+Order of setup operations:
+# 1. Create new fossil repo for the rebuild:
+    cd ~/dev/fossil/; #fossil repo dir
+#2 Fossil init command:
+fossil init {rebuild}.fossil
+--date-override DATETIME # sourced from init commit from original repo
+--admin-user USERNAME # the name configured for use with github account
+--template ./source_repo.fossil #to match existing config
+--project-name # copy from source repo
+--project-desc #copy from source repo
+
+#2 The init only defines the admin-user, but a default user remains
+necessary and undefined so far. Set the default user:
+fossil user default USERNAME -R {rebuild}.fossil
+
+#3 Update the default user with the correct contact mail
+## This will match the user name and email used with github for the repo.
+fossil user contact USERNAME contact@email.com -R {rebuild}.fossil
+
+Functions:
+    new_repo:
+    defualt_user:
+    user_contact:
+"""
+
+import subprocess
+
+from quarryforge import model
+from quarryforge.config import fossil_config as _
+from quarryforge.config.exception_conf import exception_config as ec
+from quarryforge.config.exception_conf import exception_data as e_data
+from quarryforge.config.exception_conf import (
+    fossil_exception_config as fossil_ec,
+)
+from quarryforge.exception import fossil_exception
+from quarryforge.util import fossil_util
+
+
+def new_repo(
+    username: str,
+    date_override: str,
+    new_repo: model.FossilRepo,
+    template: model.FossilRepo | None,
+    project_name: str | None,
+    project_desc: str | None,
+) -> str:
+    """Run the new repository command to configure a new repository.
+
+    Args:
+        username (str): The admin username for the new repository.
+        date_override (str):
+            The date and time string to override the initial commit date.
+        new_repo (model.FossilRepo):
+            The FossilRepo object for the new repository.
+        template (Optional[model.FossilRepo]):
+            An optional template repository to copy config from.
+        project_name (Optional[str]): Optional project name.
+        project_desc (Optional[str]): Optional project description.
+
+    Returns:
+        str: The stdout from the fossil init command.
+
+    Raises:
+        fossil_exception.FossilProcessError:
+            If the Fossil command fails.
+        fossil_exception.FossilTimeoutError:
+            If the Fossil command times out.
+        fossil_exception.FossilSetupError:
+            If there's an issue specific to repository setup.
+
+    """
+    try:
+        init_repo: subprocess.CompletedProcess[bytes] = subprocess.run(
+            fossil_util.rebuild_init(
+                username,
+                date_override,
+                new_repo,
+                template,
+                project_name,
+                project_desc,
+            ),
+            cwd=new_repo.file.parent,
+            capture_output=True,
+            check=True,
+            timeout=int(_.Fossil.DEFAULT_TIMEOUT),
+        )
+    except subprocess.CalledProcessError as error:
+        process_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_PROCESS,
+            error_code=_.Fossil.PROCESS_ERROR,
+            arg=str(new_repo),
+            extra_details={
+                _.Fossil.CMD: error.cmd,
+                _.Fossil.RETURN_CODE: error.returncode,
+                _.Fossil.OUTPUT: error.stdout,
+                _.Fossil.STDERR: error.stderr,
+            },
+        )
+        process_data = process_builder.data()
+        process_error = fossil_exception.FossilProcessError(
+            **process_data.to_exception()
+        )
+        setup_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_SETUP,
+            error_code=ec.GenericError.EXTERNAL_DEPENDENCY_ERROR,
+            arg=str(new_repo),
+            extra_details={
+                ec.DescMsg.DEPENDENCY: process_error.details[_.Fossil.CMD],
+                ec.DescMsg.REASON: process_error.details[_.Fossil.STDERR],
+                _.Fossil.STEP: _.Fossil.INIT,
+            },
+        )
+        setup_data = setup_builder.data()
+        raise fossil_exception.FossilSetupError(
+            **setup_data.to_exception()
+        ) from process_error
+    except subprocess.TimeoutExpired as error:
+        timeout_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_TIMEOUT,
+            error_code=_.Fossil.TIMEOUT_ERROR,
+            arg=str(new_repo),
+            extra_details={
+                _.Fossil.ARGS: error.args,
+                _.Fossil.CMD: error.cmd,
+                _.Fossil.TIMEOUT: error.timeout,
+                _.Fossil.OUTPUT: error.stdout,
+                _.Fossil.STDERR: error.stderr,
+            },
+            info=str(error),
+        )
+        timeout_data = timeout_builder.data()
+        timeout_error = fossil_exception.FossilTimeoutError(
+            **timeout_data.to_exception()
+        )
+        setup_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_SETUP,
+            error_code=ec.GenericError.INVALID_STATE_ERROR,
+            arg=str(new_repo),
+            extra_details={
+                _.Fossil.ARGS: timeout_error.details[_.Fossil.ARGS],
+                _.Fossil.CMD: timeout_error.details[_.Fossil.CMD],
+                _.Fossil.TIMEOUT: timeout_error.details[_.Fossil.TIMEOUT],
+                _.Fossil.STEP: _.Fossil.INIT,
+            },
+            info=timeout_error.details[e_data.builder_config().info],
+        )
+        setup_data = setup_builder.data()
+        raise fossil_exception.FossilSetupError(
+            **setup_data.to_exception()
+        ) from timeout_error
+
+    return init_repo.stdout.decode()
+
+
+def default_user(username: str, new_repo: model.FossilRepo) -> str:
+    """Run the fossil user set default user command.
+
+    Args:
+        username (str): The username to set as default.
+        new_repo (model.FossilRepo): The repository to configure.
+
+    Returns:
+        str: The stdout from the fossil user default command.
+
+    Raises:
+        fossil_exception.FossilProcessError:
+            If the Fossil command fails.
+        fossil_exception.FossilTimeoutError:
+            If the Fossil command times out.
+        fossil_exception.FossilSetupError:
+            If there's an issue specific to setting the default user.
+
+    """
+    try:
+        default_user: subprocess.CompletedProcess[bytes] = subprocess.run(
+            fossil_util.set_default_user(username, new_repo),
+            cwd=new_repo.file.parent,
+            capture_output=True,
+            check=True,
+            timeout=int(_.Fossil.DEFAULT_TIMEOUT),
+        )
+    except subprocess.CalledProcessError as error:
+        process_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_PROCESS,
+            error_code=_.Fossil.PROCESS_ERROR,
+            arg=str(new_repo),
+            extra_details={
+                _.Fossil.CMD: error.cmd,
+                _.Fossil.RETURN_CODE: error.returncode,
+                _.Fossil.OUTPUT: error.stdout,
+                _.Fossil.STDERR: error.stderr,
+            },
+        )
+        process_data = process_builder.data()
+        process_error = fossil_exception.FossilProcessError(
+            **process_data.to_exception()
+        )
+        setup_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_SETUP,
+            error_code=ec.GenericError.EXTERNAL_DEPENDENCY_ERROR,
+            arg=str(new_repo),
+            extra_details={
+                ec.DescMsg.DEPENDENCY: process_error.details[_.Fossil.CMD],
+                ec.DescMsg.REASON: process_error.details[_.Fossil.STDERR],
+                _.Fossil.STEP: _.Fossil.USERNAME_SETUP,
+            },
+        )
+        setup_data = setup_builder.data()
+        raise fossil_exception.FossilSetupError(
+            **setup_data.to_exception()
+        ) from process_error
+    except subprocess.TimeoutExpired as error:
+        timeout_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_TIMEOUT,
+            error_code=_.Fossil.TIMEOUT_ERROR,
+            arg=str(new_repo),
+            info=str(error),
+            extra_details={
+                _.Fossil.ARGS: error.args,
+                _.Fossil.CMD: error.cmd,
+                _.Fossil.TIMEOUT: error.timeout,
+                _.Fossil.OUTPUT: error.stdout,
+                _.Fossil.STDERR: error.stderr,
+            },
+        )
+        timeout_data = timeout_builder.data()
+        timeout_error = fossil_exception.FossilTimeoutError(
+            **timeout_data.to_exception()
+        )
+        setup_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_SETUP,
+            error_code=ec.GenericError.INVALID_STATE_ERROR,
+            arg=str(new_repo),
+            info=timeout_error.details[e_data.builder_config().info],
+            extra_details={
+                _.Fossil.ARGS: timeout_error.details[_.Fossil.ARGS],
+                _.Fossil.CMD: timeout_error.details[_.Fossil.CMD],
+                _.Fossil.TIMEOUT: timeout_error.details[_.Fossil.TIMEOUT],
+                _.Fossil.STEP: _.Fossil.USERNAME_SETUP,
+            },
+        )
+        setup_data = setup_builder.data()
+        raise fossil_exception.FossilSetupError(
+            **setup_data.to_exception()
+        ) from timeout_error
+
+    return default_user.stdout.decode()
+
+
+def user_contact(username: str, email: str, source: model.FossilRepo) -> str:
+    """Run the fossil user contact command.
+
+    Args:
+        username (str): The username whose contact info to update.
+        email (str): The new contact email address.
+        source (model.FossilRepo): The repository to configure.
+
+    Returns:
+        str: The stdout from the fossil user contact command.
+
+    Raises:
+        fossil_exception.FossilProcessError:
+            If the Fossil command fails.
+        fossil_exception.FossilTimeoutError:
+            If the Fossil command times out.
+        fossil_exception.FossilSetupError:
+            If there's an issue specific to setting user contact info.
+
+    """
+    try:
+        user_contact: subprocess.CompletedProcess[bytes] = subprocess.run(
+            fossil_util.set_user_contact(username, email, source),
+            cwd=source.file.parent,
+            capture_output=True,
+            check=True,
+            timeout=int(_.Fossil.DEFAULT_TIMEOUT),
+        )
+    except subprocess.CalledProcessError as error:
+        process_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_PROCESS,
+            error_code=_.Fossil.PROCESS_ERROR,
+            arg=str(source),
+            extra_details={
+                _.Fossil.CMD: error.cmd,
+                _.Fossil.RETURN_CODE: error.returncode,
+                _.Fossil.OUTPUT: error.stdout,
+                _.Fossil.STDERR: error.stderr,
+            },
+        )
+        process_data = process_builder.data()
+        process_error = fossil_exception.FossilProcessError(
+            **process_data.to_exception()
+        )
+        setup_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_SETUP,
+            error_code=ec.GenericError.EXTERNAL_DEPENDENCY_ERROR,
+            arg=str(source),
+            extra_details={
+                ec.DescMsg.DEPENDENCY: process_error.details[_.Fossil.CMD],
+                ec.DescMsg.REASON: process_error.details[_.Fossil.STDERR],
+                _.Fossil.STEP: _.Fossil.USER_CONTACT,
+            },
+        )
+        setup_data = setup_builder.data()
+        raise fossil_exception.FossilSetupError(
+            **setup_data.to_exception()
+        ) from process_error
+    except subprocess.TimeoutExpired as error:
+        timeout_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_TIMEOUT,
+            error_code=_.Fossil.TIMEOUT_ERROR,
+            arg=str(source),
+            info=str(error),
+            extra_details={
+                _.Fossil.ARGS: error.args,
+                _.Fossil.CMD: error.cmd,
+                _.Fossil.TIMEOUT: error.timeout,
+                _.Fossil.OUTPUT: error.stdout,
+                _.Fossil.STDERR: error.stderr,
+            },
+        )
+        timeout_data = timeout_builder.data()
+        timeout_error = fossil_exception.FossilTimeoutError(
+            **timeout_data.to_exception()
+        )
+        setup_builder = fossil_ec.FossilErrorBuilder(
+            error_context=fossil_ec.FossilErrorPath.FOSSIL_SETUP,
+            error_code=ec.GenericError.INVALID_STATE_ERROR,
+            arg=str(source),
+            info=timeout_error.details[e_data.builder_config().info],
+            extra_details={
+                _.Fossil.ARGS: timeout_error.details[_.Fossil.ARGS],
+                _.Fossil.CMD: timeout_error.details[_.Fossil.CMD],
+                _.Fossil.TIMEOUT: timeout_error.details[_.Fossil.TIMEOUT],
+                _.Fossil.STEP: _.Fossil.USER_CONTACT,
+            },
+        )
+        setup_data = setup_builder.data()
+        raise fossil_exception.FossilSetupError(
+            **setup_data.to_exception()
+        ) from timeout_error
+
+    return user_contact.stdout.decode()
