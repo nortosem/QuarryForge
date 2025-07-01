5523a05c820f989ce32990240bbfb6d0f755423c
mesotron.dev <mail@mesotron.dev>
2025-07-01 21:27:47 +0000

    update for refactor.
    FossilOrigin-Name: b8fe526ae38ff90936fd101c255e4ae8a4ca0a8b0536814bd3c31b063198a385

--- tests/test_quarryforge/test_config/test_meta_config.py
+++ tests/test_quarryforge/test_config/test_meta_config.py
@@ -1,6 +1,7 @@
+"""Unit tests for the quarryforge.config.meta_config module."""
+
 from quarryforge.config import meta_config
 from quarryforge.config.exception_conf import exception_data
-from tests.test_config.test_root import get_namedtuple_fields
 
 
 class TestMetaConfig:
@@ -11,30 +12,25 @@ class TestMetaConfig:
         assert hasattr(meta_config, '__all__')
         assert meta_config.__all__ == []
 
-    def test_attr_mod_type_config_attributes(self):
-        """Test attributes of AttrModTypeConfig."""
-        cfg = meta_config.MOD_TYPE
+    def test_attribute_modifier_type_factory(self):
+        """Test the attribute_modifier_type factory and its return object."""
+        cfg = meta_config.attribute_modifier_type()
         assert isinstance(cfg, meta_config.AttrModTypeConfig)
-
         assert cfg.set_attribute == 'set attribute'
         assert cfg.delete_attribute == 'delete attribute'
 
-        expected_fields = ['set_attribute', 'delete_attribute']
-        assert sorted(
-            get_namedtuple_fields(meta_config.AttrModTypeConfig)
-        ) == sorted(expected_fields)
-
-    def test_error_builder_config_attributes(self):
-        """Test attributes of ErrorBuilderConfig."""
-        cfg = meta_config.BUILDER_CONFIG
+    def test_error_builder_factory(self):
+        """Test the error_builder factory and its return object."""
+        cfg = meta_config.error_builder()
         assert isinstance(cfg, meta_config.ErrorBuilderConfig)
 
-        assert cfg.error_context == exception_data.BUILDER_FIELD.error_context
-        assert cfg.error_code == exception_data.BUILDER_FIELD.error_code
-        assert cfg.arg == exception_data.BUILDER_FIELD.arg
-        assert cfg.extra_details == exception_data.BUILDER_FIELD.extra_details
-        assert cfg.info == exception_data.BUILDER_FIELD.info
-        assert cfg.field == exception_data.BUILDER_FIELD.field
+        builder_fields = exception_data.builder_config()
+        assert cfg.error_context == builder_fields.error_context
+        assert cfg.error_code == builder_fields.error_code
+        assert cfg.arg == builder_fields.arg
+        assert cfg.extra_details == builder_fields.extra_details
+        assert cfg.info == builder_fields.info
+        assert cfg.field == builder_fields.field
 
         assert cfg.error_context == 'error_context'
         assert cfg.error_code == 'error_code'
@@ -51,13 +47,4 @@ class TestMetaConfig:
             'info',
             'field',
         ]
-        assert sorted(
-            get_namedtuple_fields(meta_config.ErrorBuilderConfig)
-        ) == sorted(expected_fields)
-
-    def test_global_constants_instances(self):
-        """Test the global config instances."""
-        assert isinstance(meta_config.MOD_TYPE, meta_config.AttrModTypeConfig)
-        assert isinstance(
-            meta_config.BUILDER_CONFIG, meta_config.ErrorBuilderConfig
-        )
+        assert sorted(cfg.slots()) == sorted(expected_fields)
